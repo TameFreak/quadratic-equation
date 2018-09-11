e5acc726d9d521fec7384ec0ac57bcbc6632cce6
Alex Lesnicky <alex.lesnicky@gmail.com>
2018-09-11 16:17:03 +0300

    -_-

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  
+  const masRes = [];
+
+
+  const a =  parseInt(equation.match(/\-?\+?\d+(?= \* x\^2)/)[0], 10);
+  let b = equation.match(/\-?\+?\s\d+(?= \* x)/)[0];
+  (b.indexOf('- ') === 0) ? b = parseInt(b.replace(' ', '')) : b = parseInt(b.replace('+ ', ''));
+  let c = equation.match(/\-?\+?\s\d+(?!.)/)[0];
+  (c.indexOf('- ') === 0) ? c = parseInt(c.replace(' ', '')) : c = parseInt(c.replace('+ ', ''));
+
+
+  const D = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+  masRes.push(Math.round(((-b + D) / (2 * a))));
+  masRes.push(Math.round(((-b - D) / (2 * a))));
+
+
+  masRes.sort((a, b) => a - b);
+  return masRes;
 }
